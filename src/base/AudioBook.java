7f759d296db1159a551c32b478e0e33d940cca9a
tutor-yun <jslovejs182@gmail.com>
2023-10-31 18:07:37 +0900

    요구사항2 완료 - BM2 생성 - EBook, AudioBook 클래스 추가 - EBook과 AudioBook 타입 형식에 맞게 등록, 수정, 조회 분기

---
+++ src/base/AudioBook.java
@@ -0,0 +1,40 @@
+package base;
+
+import java.time.LocalDate;
+
+public class AudioBook extends Book {
+    String fileSize;
+    String language;
+    int playTime;
+
+    public AudioBook(Long id, String name, String author, Long isbn, LocalDate publishedDate, String fileSize, String language, int playTime) {
+        super(id, name, author, isbn, publishedDate);
+        this.fileSize = fileSize;
+        this.language = language;
+        this.playTime = playTime;
+    }
+
+    public String getFileSize() {
+        return fileSize;
+    }
+
+    public void setFileSize(String fileSize) {
+        this.fileSize = fileSize;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public int getPlayTime() {
+        return playTime;
+    }
+
+    public void setPlayTime(int playTime) {
+        this.playTime = playTime;
+    }
+}
--- src/base/Execute.java
+++ src/base/Execute.java
@@ -2,7 +2,7 @@ package base;
 
 public class Execute {
 
-    static BookManager bm = new BM();
+    static BookManager bm = new BM2();
 
     public static void main(String[] args) {
         bm.init();
